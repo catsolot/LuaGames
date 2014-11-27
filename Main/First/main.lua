f7c46a4dab8f627c12f4cd0689de585291f513c1
catsolot <milocraun@gmail.com>
2014-11-26 20:59:43 -0500

    Added movement

--- Main/First/main.lua
+++ Main/First/main.lua
@@ -1,11 +1,34 @@
 function love.load()
 man = love.graphics.newImage("Man.png")
+x = 50
+y = 50
+end
+
+function keys()
+left = love.keyboard.isDown('a', "left")
+up = love.keyboard.isDown('w')
+down = love.keyboard.isDown('s')
+right = love.keyboard.isDown('d')
 end
 
 function love.draw()
-love.graphics.draw(man, 50, 50)
+love.graphics.draw(man, x, y)
+print(x, y)
 end
 
-function love.update()
+function love.update(dt)
+keys()
+if left then
+x = x - 5
+end
+if right then 
+x = x + 5
+end
+if up then
+y = y - 5
+end
+if down then 
+y = y + 5
+end
 end
 
